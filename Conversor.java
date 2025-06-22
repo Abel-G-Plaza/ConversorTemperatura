1cf0621753e7537a584b4fe39de54f8d94c8d6fb
Abel González Plaza <abgp2412@gmail.com>
2025-06-22 10:49:36 +0200

    Añadido método main de prueba

--- Conversor.java
+++ Conversor.java
@@ -7,4 +7,13 @@ public static double fahrenheitACelsius (double fahrenheit) {
 return (fahrenheit - 32) * 5/9;
 }
 
+public static void main(String[] args) {
+double c = 25;
+double f = celsiusAFahrenheit(c);
+System.out.println(c + " °C son " + f + " F");
+f = 77;
+c = fahrenheitACelsius (f);
+System.out.println(f + " F son " + c + " °C");
+
+}
 
