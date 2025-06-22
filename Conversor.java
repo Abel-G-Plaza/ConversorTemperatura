b440788546331be6ed453e6e8397e4f2eb7904fd
Abel González Plaza <abgp2412@gmail.com>
2025-06-22 10:47:10 +0200

    Commit inicial Funciones de conversión

---
+++ Conversor.java
@@ -0,0 +1,10 @@
+public class Conversor {
+public static double celsiusAFahrenheit(double celsius) {
+return (celsius * 9/5) + 32;
+}
+
+public static double fahrenheitACelsius (double fahrenheit) {
+return (fahrenheit - 32) * 5/9;
+}
+
+
